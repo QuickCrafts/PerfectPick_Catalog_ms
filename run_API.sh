1673f1fca1f1afd28d4a73cf969c64eb9e3e5f3f
Paula Guzmán <pauladaguz@gmail.com>
2024-05-04 11:45:59 -0500

    Advance Automatic

--- run_API.sh
+++ run_API.sh
@@ -1,12 +1,21 @@
+echo "Delete existing Catalog container"
+
+docker stop perfectpick_catalog_ms
+docker rm perfectpick_catalog_ms
+
+echo "Delete existing Catalog image"
+
+docker rmi perfectpick_catalog_ms
+
+echo "Up Cassandra DB server"
 #Docker cassandra
 docker pull cassandra
 
+echo "Build image Catalog"
 #Docker catalog ms
 docker build -t perfectpick_catalog_ms .
 
-#Docker network
-docker network create perfectpick_catalog_nw
-
+echo "running docker..."
 #Docker network containers
-docker run --name perfectpick_catalog_db --network perfectpick_catalog_nw -p 9842:9842 -d cassandra
-docker run --name perfectpick_catalog_ms --network perfectpick_catalog_nw -p 8001:8001 -d perfectpick_catalog_ms
+docker run --name perfectpick_catalog_db --network perfectpicknetwork -p 9842:9842 -d cassandra
+docker run --name perfectpick_catalog_ms --network perfectpicknetwork -p 8001:8001 -d perfectpick_catalog_ms
